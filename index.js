30c8d5f87f1a5acae9a69cff1da0bafabf6d2691
Jaiden-Wilson <96029895+Jaiden-Wilson@users.noreply.github.com>
2022-01-28 21:24:37 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -4,19 +4,21 @@ import { Menu } from "antd";
 import MenuItem from "antd/lib/menu/MenuItem";
 import "antd/dist/antd.css";
 import { ButtonTracker } from "./pageNav";
+import { Table1 } from "./Table";
 
 //Variable Declarations and Initializations
 var userInput = null,
-  listLength = 5,
+  listLength = 10,
   storeNames = [],
-  selectedRows = [];
-var rowArray = [];
+  selectedRows = [],
+  rowArray = [],
+  locationData = [];
 
 ReactDOM.render(
   <div id="parentContainer">
     <h1 className="title">Top Shopify Stores</h1>
     <input type="search" id="searchField" />
-    <button id="searchButton">Search</button>
+
     <div id="sideMenu">
       <Menu className="beamInfo" mode="inline" theme="dark">
         <MenuItem key="firstItem">
@@ -30,6 +32,8 @@ ReactDOM.render(
         </MenuItem>
       </Menu>
     </div>
+    <Table1 />
+
     <div id="tableContainer">
       <ButtonTracker />
     </div>
@@ -40,14 +44,12 @@ ReactDOM.render(
 for (var i = 0; i < listLength; i++) {
   storeNames.push(document.getElementsByClassName("Store")[i].innerText);
   rowArray.push(document.getElementsByClassName("dataRow")[i]);
+  locationData.push(document.getElementsByClassName("location")[i].innerText);
 }
-//Button Activations
-document
-  .getElementById("searchButton")
-  .addEventListener("click", buttonClicked);
 
-//Button response method
-function buttonClicked() {
+//Button response methods
+function searchButtonClicked() {
+  selectedRows = [];
   userInput = document.getElementById("searchField").value;
   for (var i = 0; i < listLength; i++) {
     if (contains(userInput, storeNames[i]) === true) {
@@ -55,18 +57,42 @@ function buttonClicked() {
     }
   }
 
-  document.getElementById("shopifyData").setInnerHTML =
-    rowArray[selectedRows[0]];
+  var searchResults = [];
+  for (var k = 0; k < selectedRows.length; k++) {
+    searchResults.push(rowArray[selectedRows[k]]);
+  }
+  return searchResults;
 }
 
 function contains(s1, s2) {
   var contains = false;
-  for (var i = 0; i < s2.length - 1; i++) {
-    for (var j = i + 1; j < s2.length; j++) {
-      if (s1 === s2.slice(i, j)) {
+  for (var i = 0; i < s2.length; i++) {
+    for (var j = i + 1; j < s2.length + 1; j++) {
+      if (s1.toLowerCase() === s2.slice(i, j).toLowerCase()) {
         contains = true;
       }
     }
   }
   return contains;
 }
+function filter() {
+  selectedRows = [];
+  var m = document.getElementById("filterMenu").selectedKeys;
+  var l = m.length;
+  for (var i = 0; i < l; i++) {
+    for (var j = 0; j < listLength; j++) {
+      if (m[i] === locationData[j]) {
+        selectedRows.push(j);
+      }
+    }
+  }
+  var filterResults = [];
+  for (var k = 0; k < selectedRows.length; k++) {
+    filterResults.push(rowArray[selectedRows[k]]);
+  }
+  return filterResults;
+}
+function getDataBase() {
+  return rowArray;
+}
+export { searchButtonClicked, getDataBase, filter };
--- pageNav.jsx
+++ pageNav.jsx
@@ -1,361 +1,251 @@
 import React from "react";
-var s = { btnNum: 2 },
-  t = (
-    <table cellPadding="30px" id="shopifyData">
-      <thead>
-        <tr>
-          <th>Rank</th>
-          <th>Store</th>
-          <th>Yearly Revenue</th>
-          <th>Country/Region</th>
-          <th>Monthly Traffic</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          className="dataRow"
-          height="31"
-          role="button"
-          title="Check the BBC Shop US store"
-        >
-          <td>1</td>
-          <td className="Store">
-            <a href="https://shop.bbc.com/">BBC Shop US</a>
-          </td>
-          <td>-</td>
-          <td>United States</td>
-          <td>416,700,000</td>
-        </tr>
-        <tr
-          className="dataRow"
-          height="31"
-          role="button"
-          title="Check the Gymshark ROW store"
-        >
-          <td>2</td>
-          <td className="Store">
-            <a href="https://row.gymshark.com/">Gymshark ROW</a>
-          </td>
-          <td className="xl87" width="232">
-            -
-          </td>
-          <td className="xl86" width="64">
-            United Kingdom
-          </td>
-          <td className="xl88" width="64">
-            22,800,000
-          </td>
-        </tr>
-        <tr
-          className="dataRow"
-          height="31"
-          role="button"
-          title="Check the Gymshark UK store"
-        >
-          <td>3</td>
-          <td className="Store">
-            <a href="https://uk.gymshark.com/">Gymshark UK</a>
-          </td>
-          <td className="xl87" width="232">
-            -
-          </td>
-          <td className="xl86" width="64">
-            United Kingdom
-          </td>
-          <td className="xl88" width="64">
-            22,800,000
-          </td>
-        </tr>
-        <tr
-          className="dataRow"
-          height="31"
-          role="button"
-          title="Check the Gymshark US store"
-        >
-          <td>4</td>
-          <td className="Store">
-            <a href="https://row.gymshark.com/">Gymshark US</a>
-          </td>
-          <td className="xl87" width="232">
-            -
-          </td>
-          <td className="xl86" width="64">
-            United States
-          </td>
-          <td className="xl88" width="64">
-            22,800,000
-          </td>
-        </tr>
-        <tr
-          className="dataRow"
-          height="20"
-          role="button"
-          title="Check the JB Hi-Fi store"
-        >
-          <td height="20" className="xl84" width="64">
-            5
-          </td>
-          <td className="Store">
-            <a href="https://www.jbhifi.com.au/">JB Hi-Fi</a>
-          </td>
-          <td className="xl87" width="232">
-            -
-          </td>
-          <td className="xl86" width="64">
-            Australia
-          </td>
-          <td className="xl88" width="64">
-            13,500,000
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  );
+import { searchButtonClicked, getDataBase, filter } from "./index";
+import { Dropdown, Menu } from "antd";
+import "antd/dist/antd.css";
+import MenuItem from "antd/lib/menu/MenuItem";
+var t = (
+  <table cellPadding="30px" id="shopifyData">
+    <thead>
+      <tr>
+        <th>Rank</th>
+        <th>Store</th>
+        <th>Yearly Revenue</th>
+        <th>Country/Region</th>
+        <th>Monthly Traffic</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        className="dataRow"
+        height="31"
+        role="button"
+        title="Check the BBC Shop US store"
+      >
+        <td>1</td>
+        <td className="Store">
+          <a href="https://shop.bbc.com/">BBC Shop US</a>
+        </td>
+        <td>-</td>
+        <td className="location">United States</td>
+        <td>416,700,000</td>
+      </tr>
+      <tr
+        className="dataRow"
+        height="31"
+        role="button"
+        title="Check the Gymshark ROW store"
+      >
+        <td>2</td>
+        <td className="Store">
+          <a href="https://row.gymshark.com/">Gymshark ROW</a>
+        </td>
+        <td className="xl87" width="232">
+          -
+        </td>
+        <td className="location" width="64">
+          United Kingdom
+        </td>
+        <td className="xl88" width="64">
+          22,800,000
+        </td>
+      </tr>
+      <tr
+        className="dataRow"
+        height="31"
+        role="button"
+        title="Check the Gymshark UK store"
+      >
+        <td>3</td>
+        <td className="Store">
+          <a href="https://uk.gymshark.com/">Gymshark UK</a>
+        </td>
+        <td className="xl87" width="232">
+          -
+        </td>
+        <td className="location" width="64">
+          United Kingdom
+        </td>
+        <td className="xl88" width="64">
+          22,800,000
+        </td>
+      </tr>
+      <tr
+        className="dataRow"
+        height="31"
+        role="button"
+        title="Check the Gymshark US store"
+      >
+        <td>4</td>
+        <td className="Store">
+          <a href="https://row.gymshark.com/">Gymshark US</a>
+        </td>
+        <td className="xl87" width="232">
+          -
+        </td>
+        <td className="location" width="64">
+          United States
+        </td>
+        <td className="xl88" width="64">
+          22,800,000
+        </td>
+      </tr>
+      <tr
+        className="dataRow"
+        height="20"
+        role="button"
+        title="Check the JB Hi-Fi store"
+      >
+        <td height="20" className="xl84" width="64">
+          5
+        </td>
+        <td className="Store">
+          <a href="https://www.jbhifi.com.au/">JB Hi-Fi</a>
+        </td>
+        <td className="xl87" width="232">
+          -
+        </td>
+        <td className="location" width="64">
+          Australia
+        </td>
+        <td className="xl88" width="64">
+          13,500,000
+        </td>
+      </tr>
+    </tbody>
+  </table>
+);
+var rowsPerPage = 5;
 
 class ButtonTracker extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { btnNum: 2 };
+    this.state = { btnNum: 1 };
   }
   one = () => {
+    var a = getDataBase(),
+      c = document.getElementById("shopifyData");
+    var d = c.rows.length;
+    //Clear Previous Rows
+    for (var j = 0; j < d - 1; j++) {
+      c.deleteRow(-1);
+    }
+    //Repopulate Table
+    for (var k = 0; k < rowsPerPage; k++) {
+      c.insertRow(k + 1).innerHTML = a[k].innerHTML;
+    }
     this.setState({ btnNum: 1 });
-    s = this.state;
-    t = (
-      <table cellPadding="30px" id="shopifyData">
-        <thead>
-          <tr>
-            <th>Rank</th>
-            <th>Store</th>
-            <th>Yearly Revenue</th>
-            <th>Country/Region</th>
-            <th>Monthly Traffic</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            className="dataRow"
-            height="31"
-            role="button"
-            title="Check the BBC Shop US store"
-          >
-            <td>1</td>
-            <td className="Store">
-              <a href="https://shop.bbc.com/">BBC Shop US</a>
-            </td>
-            <td>-</td>
-            <td>United States</td>
-            <td>416,700,000</td>
-          </tr>
-          <tr
-            className="dataRow"
-            height="31"
-            role="button"
-            title="Check the Gymshark ROW store"
-          >
-            <td>2</td>
-            <td className="Store">
-              <a href="https://row.gymshark.com/">Gymshark ROW</a>
-            </td>
-            <td className="xl87" width="232">
-              -
-            </td>
-            <td className="xl86" width="64">
-              United Kingdom
-            </td>
-            <td className="xl88" width="64">
-              22,800,000
-            </td>
-          </tr>
-          <tr
-            className="dataRow"
-            height="31"
-            role="button"
-            title="Check the Gymshark UK store"
-          >
-            <td>3</td>
-            <td className="Store">
-              <a href="https://uk.gymshark.com/">Gymshark UK</a>
-            </td>
-            <td className="xl87" width="232">
-              -
-            </td>
-            <td className="xl86" width="64">
-              United Kingdom
-            </td>
-            <td className="xl88" width="64">
-              22,800,000
-            </td>
-          </tr>
-          <tr
-            className="dataRow"
-            height="31"
-            role="button"
-            title="Check the Gymshark US store"
-          >
-            <td>4</td>
-            <td className="Store">
-              <a href="https://row.gymshark.com/">Gymshark US</a>
-            </td>
-            <td className="xl87" width="232">
-              -
-            </td>
-            <td className="xl86" width="64">
-              United States
-            </td>
-            <td className="xl88" width="64">
-              22,800,000
-            </td>
-          </tr>
-          <tr
-            className="dataRow"
-            height="20"
-            role="button"
-            title="Check the JB Hi-Fi store"
-          >
-            <td height="20" className="xl84" width="64">
-              5
-            </td>
-            <td className="Store">
-              <a href="https://www.jbhifi.com.au/">JB Hi-Fi</a>
-            </td>
-            <td className="xl87" width="232">
-              -
-            </td>
-            <td className="xl86" width="64">
-              Australia
-            </td>
-            <td className="xl88" width="64">
-              13,500,000
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    );
   };
   two = () => {
-    this.setState({ btnNum: 2 });
-    s = this.state;
-    t = (
-      <table cellPadding="30px" id="shopifyData">
-        <thead>
-          <tr>
-            <th>Rank</th>
-            <th>Store</th>
-            <th>Yearly Revenue</th>
-            <th>Country/Region</th>
-            <th>Monthly Traffic</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            className="dataRow"
-            height="31"
-            role="button"
-            title="Check the Cettire store"
-          >
-            <td>6</td>
-            <td className="Store">
-              <a href="https://www.cettire.com/">Cettire</a>
-            </td>
-            <td>-</td>
-            <td>United States</td>
-            <td>24,400,000</td>
-          </tr>
-          <tr
-            className="dataRow"
-            height="31"
-            role="button"
-            title="Check the ColourPop store"
-          >
-            <td>7</td>
-            <td className="Store">
-              <a href="https://colourpop.com/">ColourPop</a>
-            </td>
-            <td className="xl87" width="232">
-              -
-            </td>
-            <td className="xl86" width="64">
-              United States
-            </td>
-            <td className="xl88" width="64">
-              4,280,000
-            </td>
-          </tr>
-          <tr
-            className="dataRow"
-            height="31"
-            role="button"
-            title="Check the Puravida store"
-          >
-            <td>8</td>
-            <td className="Store">
-              <a href="https://www.puravidabracelets.com/">
-                Pura Vida Bracelets
-              </a>
-            </td>
-            <td className="xl87" width="232">
-              -
-            </td>
-            <td className="xl86" width="64">
-              United States
-            </td>
-            <td className="xl88" width="64">
-              17,600,000
-            </td>
-          </tr>
-          <tr
-            className="dataRow"
-            height="31"
-            role="button"
-            title="Check the Beeinspired store"
-          >
-            <td>9</td>
-            <td className="Store">
-              <a href="https://www.beeinspiredclothing.com/">
-                Bee Inspired Clothing
-              </a>
-            </td>
-            <td className="xl87" width="232">
-              -
-            </td>
-            <td className="xl86" width="64">
-              United Kingdom
-            </td>
-            <td className="xl88" width="64">
-              14,500,000
-            </td>
-          </tr>
-          <tr
-            className="dataRow"
-            height="20"
-            role="button"
-            title="Check the Staples store"
-          >
-            <td height="20" className="xl84" width="64">
-              10
-            </td>
-            <td className="Store">
-              <a href="https://www.staples.ca/">Staples</a>
-            </td>
-            <td className="xl87" width="232">
-              -
-            </td>
-            <td className="xl86" width="64">
-              Canada
-            </td>
-            <td className="xl88" width="64">
-              7,460,000
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    );
-  };
+    var a = getDataBase(),
+      c = document.getElementById("shopifyData");
+    var d = c.rows.length;
+    //Clear Previous Rows
+    for (var j = 0; j < d - 1; j++) {
+      c.deleteRow(-1);
+    }
+    //Repopulate Table
+    for (var k = 0; k < rowsPerPage; k++) {
+      c.insertRow(k + 1).innerHTML = a[k + 5].innerHTML;
+    }
 
+    this.setState({ btnNum: 2 });
+  };
+  search = () => {
+    var a = searchButtonClicked();
+    var c = document.getElementById("shopifyData");
+    var h = c.rows.length;
+    //c.innerHTML = "";
+    for (var d = 0; d < h - 1; d++) {
+      c.deleteRow(-1);
+    }
+    for (var b = 0; b < a.length; b++) {
+      c.insertRow(b + 1).innerHTML = a[b].innerHTML;
+    }
+    this.setState({ btnNum: 0 });
+    console.log(a);
+  };
+  select = () => {
+    var b = document.getElementById("applyFilters");
+    b.style.visibility = "visible";
+    console.log(document.getElementById("filterMenu").selectedKeys);
+    this.setState({ btnNum: 3 });
+  };
+  applyFilters = () => {
+    var a = filter(),
+      c = document.getElementById("shopifyData");
+    var d = c.rows.length;
+    //Clear Previous Rows
+    for (var j = 0; j < d - 1; j++) {
+      c.deleteRow(-1);
+    }
+    //Repopulate Table
+    for (var k = 0; k < a.length; k++) {
+      c.insertRow(k + 1).innerHTML = a[k].innerHTML;
+    }
+    this.setState({ btnNum: 4 });
+  };
+  item1Chosen = () => {
+    document.getElementById("filterMenu").selectedKeys = ["United States"];
+  };
+  item2Chosen = () => {
+    document.getElementById("filterMenu").selectedKeys = ["United Kingdom"];
+  };
+  item3Chosen = () => {
+    document.getElementById("filterMenu").selectedKeys = ["Canada"];
+  };
+  item4Chosen = () => {
+    document.getElementById("filterMenu").selectedKeys = ["Australia"];
+  };
   render() {
     return (
       <div>
+        <button onClick={this.search} id="searchButton">
+          Search
+        </button>
         {t}
+        <Dropdown
+          overlay={
+            <Menu onSelect={this.select} selectable id="filterMenu">
+              <MenuItem
+                onClick={this.item1Chosen}
+                className="choice"
+                key="United States"
+              >
+                United States
+              </MenuItem>
+              <MenuItem
+                onClick={this.item2Chosen}
+                className="choice"
+                key="United Kingdom"
+              >
+                United Kingdom
+              </MenuItem>
+              <MenuItem
+                onClick={this.item3Chosen}
+                className="choice"
+                key="Canada"
+              >
+                Canada
+              </MenuItem>
+              <MenuItem
+                onClick={this.item4Chosen}
+                className="choice"
+                key="Australia"
+              >
+                Australia
+              </MenuItem>
+            </Menu>
+          }
+        >
+          <button id="filter">Filter By Region</button>
+        </Dropdown>
+        <button
+          onClick={this.applyFilters}
+          className="applyFilters"
+          id="applyFilters"
+        >
+          Apply Filter
+        </button>
         <div id="pageNav">
           <button id="Button1" onClick={this.one}>
             1
@@ -368,7 +258,5 @@ class ButtonTracker extends React.Component {
     );
   }
 }
-function getState() {
-  return s;
-}
-export { ButtonTracker, getState };
+
+export { ButtonTracker };
